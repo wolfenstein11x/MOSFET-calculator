c2a350fd4c94342ef61831682b36a3cb92a58655
Charles <c.wolfe132@gmail.com>
2020-01-07 21:22:41 -0800

    initial commit

---
+++ MOSFET.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "stdafx.h"
+#include <iostream>
+
+bool nmos_or_pmos();
+
+struct NMOS
+{
+	// adjust these constants as needed
+	const double Vtn = 1.0;
+	const double kn_ = 0.00008;
+	const double W = 0.00001;
+	const double L = 0.00000018;
+
+	// these values will be input by user
+	double VD;
+	double VG;
+	double VS;
+
+	// DC drain to source current is what you are solving for, initialize it to arbitrary value
+	double IDS = 0;
+
+	void get_voltages();
+	double calc_current_sat(double &current);
+	double calc_current_tri(double &current);
+	int sat_triode_cutoff();
+	bool valid_input_check();
+	void present_answer();
+
+};
+
+struct PMOS
+{
+	// adjust these constants as needed
+	const double Vtp = -1.0;
+	const double kp_ = 0.00002;
+	const double W = 0.00001;
+	const double L = 0.00000018;
+
+	// these values will be input by user
+	double VD;
+	double VG;
+	double VS;
+
+	// DC source to drain current is what you are solving for, initialize it to arbitrary value
+	double ISD = 0;
+
+	void get_voltages();
+	double calc_current_sat(double &current);
+	double calc_current_tri(double &current);
+	int sat_triode_cutoff();
+	bool valid_input_check();
+	void present_answer();
+};
+
---
+++ MOSFET_source_code.cpp
@@ -0,0 +1,104 @@
+#include "stdafx.h"
+#include "MOSFET.h"
+
+void NMOS::get_voltages()
+{
+	std::cout << "Enter the drain voltage: ";
+	std::cin >> VD;
+	std::cout << "Enter the gate voltage: ";
+	std::cin >> VG;
+	std::cout << "Enter the source voltage: ";
+	std::cin >> VS;
+}
+
+double NMOS::calc_current_sat(double &current)
+{
+	current = 0.5*(kn_)*(W / L)*pow((VG - VS - Vtn), 2);
+	return current;
+}
+
+double NMOS::calc_current_tri(double &current)
+{
+	current = (kn_)*(W / L)*((VG - VS - Vtn)*(VD - VS) - 0.5*pow((VD - VS), 2));
+	return current;
+}
+
+void NMOS::present_answer()
+{
+	std::cout << "The DC current is " << IDS * 1000 << "mA\n";
+}
+
+int NMOS::sat_triode_cutoff()
+{
+	if (VG - VS <= Vtn) {
+		return 0;
+	}
+
+	else if (VD <= VG - Vtn) {
+		return 1;
+	}
+	
+	else {
+		return 2;
+	}
+}
+
+void PMOS::get_voltages()
+{
+	std::cout << "Enter the drain voltage: ";
+	std::cin >> VD;
+	std::cout << "Enter the gate voltage: ";
+	std::cin >> VG;
+	std::cout << "Enter the source voltage: ";
+	std::cin >> VS;
+}
+
+double PMOS::calc_current_sat(double &current)
+{
+	current = 0.5*(kp_)*(W / L)*pow((VS - VG - abs(Vtp)), 2);
+	return current;
+}
+
+double PMOS::calc_current_tri(double &current)
+{
+	current = (kp_)*(W / L)*((VS - VG - Vtp)*(VS - VD) - 0.5*pow((VS - VD), 2));
+	return current;
+}
+
+void PMOS::present_answer()
+{
+	std::cout << "The DC current is " << ISD * 1000 << "mA\n";
+}
+
+int PMOS::sat_triode_cutoff()
+{
+	if (VG - VS > Vtp) {
+		return 0;
+	}
+
+	else if (VS - VD <= VS - VG - abs(Vtp)) {
+		return 1;
+	}
+
+	else {
+		return 2;
+	}
+}
+
+bool nmos_or_pmos()
+{
+	std::cout << "NMOS or PMOS?\n\n";
+	std::cout << "[1] NMOS\n";
+	std::cout << "[2] PMOS\n";
+
+	int answer;
+	std::cin >> answer;
+
+	if (answer == 1) {
+		return true;
+	}
+
+	else {
+		return false;
+	}
+}
